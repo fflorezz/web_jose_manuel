128cfcd701ec7e6eb958c38ca0709b22905306fd
fflorezz <felipeflorezart@gmail.com>
2021-06-28 21:08:37 -0500

    added img urls to play-preview component

--- src/components/play-preview.js
+++ src/components/play-preview.js
@@ -1,16 +1,12 @@
 import React from 'react'
 
-import { StaticImage } from 'gatsby-plugin-image'
-
 const PlayPreview = ({ play, idx }) => {
+  if (!play.images) return null
+
   return (
     <div className="mb-4 grid grid-cols-2 gap-4 grid-flow-col-dense">
       <div className={idx % 2 === 0 ? '' : 'order-last'}>
-        <StaticImage
-          src="https://res.cloudinary.com/dnenhfh4v/image/upload/v1623202532/web_juan_manuel/jm-port-01-md_xz9eoc.jpg"
-          alt="retrato José Manuel Freidel"
-          aspectRatio={16 / 9}
-        />
+        <img src={play.images[0].url} alt={play.title} />
       </div>
       <div className="flex flex-col px-10 items-center justify-center bg-indigo-600 text-white  text-center cursor-pointer hover:bg-indigo-800">
         <h3 className="text-4xl font-medium">{play.title}</h3>
