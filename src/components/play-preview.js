b2bdee558a0405811f10661fa9d556fa73e8f578
fflorezz <felipeflorezart@gmail.com>
2021-06-29 21:27:04 -0500

    change image heigh on preview and hover transition

--- src/components/play-preview.js
+++ src/components/play-preview.js
@@ -6,9 +6,13 @@ const PlayPreview = ({ play, idx }) => {
   return (
     <div className="mb-4 grid grid-cols-2 gap-4 grid-flow-col-dense">
       <div className={idx % 2 === 0 ? '' : 'order-last'}>
-        <img src={play.images[0].url} alt={play.title} />
+        <img
+          src={play.images[0].url}
+          alt={play.title}
+          className="h-96 w-full object-cover"
+        />
       </div>
-      <div className="flex flex-col px-10 items-center justify-center bg-indigo-600 text-white  text-center cursor-pointer hover:bg-indigo-800">
+      <div className="flex flex-col px-10 items-center justify-center bg-indigo-600 text-white  text-center cursor-pointer hover:bg-indigo-900 transition duration-300 ease-in-out">
         <h3 className="text-4xl font-medium">{play.title}</h3>
         <div className="inline-block border-t-4 pt-6 mt-6">
           <p className="text-white text-3xl pb-0 font-medium">{play.year}</p>
