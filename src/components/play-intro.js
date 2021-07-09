7c8fe680ce3d8f4d558318bb8bc22c636ea17669
fflorezz <felipeflorezart@gmail.com>
2021-07-08 19:24:54 -0500

    Added play-intro component

---
+++ src/components/play-intro.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const PlayIntro = ({ intro }) => {
+  return (
+    <div className="mt-10 italic text-justify px-4">
+      {intro.map(fragment => {
+        return (
+          <div className="grid grid-cols-12">
+            {fragment.character ? (
+              <h5 className="text-sm font-medium text-gray-500">
+                {fragment.character + ':'}
+              </h5>
+            ) : null}
+            {fragment.paragraphs ? (
+              <div
+                className={fragment.character ? 'col-span-11' : 'col-span-12'}
+              >
+                {fragment.paragraphs.map(par => (
+                  <p className="text-sm">{par}</p>
+                ))}
+              </div>
+            ) : null}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default PlayIntro
--- src/templates/play.js
+++ src/templates/play.js
@@ -2,34 +2,14 @@ import React from 'react'
 
 import Layout from './../components/layout'
 import PlayTitle from './../components/play-title'
+import PlayIntro from './../components/play-intro'
 
 const Play = ({ pageContext: { play } }) => {
   return (
     <Layout>
       <PlayTitle>{play.title}</PlayTitle>
       <div className="text-gray-400 text-3xl font-medium mt-2">{play.year}</div>
-      <div className="mt-10 italic text-justify px-4">
-        {play.intro.map(fragment => {
-          return (
-            <div className="grid grid-cols-12">
-              {fragment.character ? (
-                <h5 className="text-sm font-medium text-gray-500">
-                  {fragment.character + ':'}
-                </h5>
-              ) : null}
-              {fragment.paragraphs ? (
-                <div
-                  className={fragment.character ? 'col-span-11' : 'col-span-12'}
-                >
-                  {fragment.paragraphs.map(par => (
-                    <p className="text-sm">{par}</p>
-                  ))}
-                </div>
-              ) : null}
-            </div>
-          )
-        })}
-      </div>
+      <PlayIntro intro={play.intro} />
       <div className="mt-10">
         {play.abstract.paragraphs.map(par => {
           return <p>{par}</p>
