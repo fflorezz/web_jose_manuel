1bd61430f090a40efa352ce6a0d74bf5392fb072
fflorezz <felipeflorezart@gmail.com>
2021-08-23 18:22:43 -0500

    fixed news preview margin

--- src/components/news-preview.js
+++ src/components/news-preview.js
@@ -5,7 +5,7 @@ const NewsPreview = ({ news, idx }) => {
   if (!news.images.length) return null
 
   return (
-    <article className="pl-0 mb-6 md:mb-4 md:grid md:grid-cols-12 md:gap-4 md:grid-flow-col-dense lg:pl-4">
+    <article className="pl-0 mb-6 md:mb-24 md:grid md:grid-cols-12 md:gap-4 md:grid-flow-col-dense lg:pl-4">
       <div className="py-4 mt-6 flex flex-col col-span-7 md:py-0 md:px-0 sm:px-0">
         <h3 className="text-gray-500 text-2xl sm:text-2xl font-bold md:text-3xl">
           {news.title}
