56c91f8bbce8b38a2a9383977bd1a5ece09f93aa
fflorezz <felipeflorezart@gmail.com>
2021-10-10 19:12:03 -0500

    Added social media links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
 import * as React from 'react'
 
 import Layout from '../components/layout'
+import SocialLinks from '../components/social-links'
 
 const IndexPage = () => {
   return (
     <Layout>
-      <div className="w-full mt-16 border-transparent">
+      <div className="w-full mt-16 border-transparent relative">
+        <SocialLinks></SocialLinks>
         <img
-          className="h-screen lg:h-full object-cover"
+          style={{ zIndex: '-10' }}
+          className="h-screen lg:h-screen object-cover relative -mt-16"
           src="https://res.cloudinary.com/dnenhfh4v/image/upload/v1623207316/web_juan_manuel/jm-hero-lg_rpkouc.jpg"
           alt="Retrato José Manuel Freidel"
         />
